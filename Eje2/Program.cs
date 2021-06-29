9921af1302021c3dec1bde73ba0c4e5657994804
caroldbt <carol123dbt@gmail.com>
2021-06-30 00:02:40 +0200

    Ejercicios1-5

---
+++ Eje2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Eje2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Ejercicio 2
+            System.Console.WriteLine("+---------Ejercicio 2---------+");
+            int N = 5;
+            double A = 4.56;
+            char C = 'a';
+            System.Console.WriteLine("{0} + {1}", N, A + " = " + (N + A).ToString("0.00000"));
+            System.Console.WriteLine("{0} - {1}", A, N + " = " + (A - N).ToString("0.00000"));
+            System.Console.WriteLine("Valor numerico del caracter a = " + Convert.ToInt32(C));
+
+        }
+    }
+}
